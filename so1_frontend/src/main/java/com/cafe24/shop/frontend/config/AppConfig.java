fd8874e5d4086fe72ba756455887dcce56270b37
Sowon Park <thdnjs957@gmail.com>
2019-08-05 20:41:53 +0900

    front 테스트 완료

--- so1_frontend/src/main/java/com/cafe24/shop/frontend/config/AppConfig.java
+++ so1_frontend/src/main/java/com/cafe24/shop/frontend/config/AppConfig.java
@@ -13,4 +13,5 @@ import com.cafe24.shop.frontend.config.app.OAuth2ClientConfig;
 @ComponentScan({"com.cafe24.shop.frontend.security", "com.cafe24.shop.frontend.service",  "com.cafe24.shop.frontend.repository", "com.cafe24.shop.frontend.aspect"})
 @Import({ AppSecurityConfig.class, OAuth2ClientConfig.class })
 public class AppConfig {
+	
 }
